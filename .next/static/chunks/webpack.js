88ad940175610c25a23715892551a7b4aca4faba
Santiago :) <santiagoespinosagiraldo1@gmail.com>
2024-07-22 22:58:12 -0500

    Remove unused webpack hot-update files and update page titles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ffdd40cd6e31cb51"; }
+/******/ 		__webpack_require__.h = function() { return "5bfc14febac424a0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -5,21 +5,41 @@ import { motion } from "framer-motion";
 import styles from "../styles";
 import { slideIn, staggerContainer, textVariant } from "../utils/motion";
 
+const Hero = () => (
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <div className="relative w-full md:-mt-[20px] -mt-[12px]">
+        <div
+          className="absolute w-full h-[300px] rounded-tl-[140px] z-[0] -top-[30px]"
+          style={{
+            background: "linear-gradient(90deg, rgba(50,60,80,0.8673844537815126) 0%, rgba(51,109,132,1) 100%)"
+          }}
+        />
 
+        <div
+          className="w-full sm:h-[500px] h-[350px] rounded-tl-[140px] z-10 relative"
+          style={{
+            background: "rgb(19,29,41)",
+            background: "linear-gradient(90deg, rgba(19,29,41,0.8673844537815126) 0%, rgba(21,79,102,1) 100%)"
+          }}
+        ></div>
 
-const Hero = () => {
-  return (
-    <div className="relative w-full h-screen">
-      <div
-        className="absolute inset-0 flex justify-center items-center z-20"
-        style={{ top: "-30px" }}
-      >
-        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
-          MagiCloud
-        </motion.h1>
-      </div>
-
-      <div
+        <div
+          className="absolute inset-0 flex justify-center items-center z-20"
+          style={{ top: "-30px" }}
+        >
+         <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+    MagiCloud
+  </motion.h1>  
+  </div>
+{/* here*/}
+<div
         className="absolute inset-0 flex justify-center items-center z-20"
         style={{ top: "-30px" }}
       >
@@ -63,8 +83,22 @@ const Hero = () => {
           />
         </motion.div>
       </div>
-    </div>
-  );
-};
+
+<a >
+  <div
+    className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-30"
+    style={{ top: "-20px" }}
+  >
+    <img
+      src="/stamp.png"
+      alt="stamp"
+      className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
+    />
+  </div>
+        </a>
+      </div>
+    </motion.div>
+  </section>
+);
 
 export default Hero;
