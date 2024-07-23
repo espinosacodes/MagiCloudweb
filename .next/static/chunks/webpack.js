2d862530ee3f9815e0700eed7d9667adefff265b
Santiago :) <santiagoespinosagiraldo1@gmail.com>
2024-07-22 23:02:26 -0500

    chore: Remove unused webpack hot-update files and update page titles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5bfc14febac424a0"; }
+/******/ 		__webpack_require__.h = function() { return "c220ad786fa075f5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
